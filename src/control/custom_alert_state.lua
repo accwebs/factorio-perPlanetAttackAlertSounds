3f45869f2236f34ee598fb1a56b68098135a696f
Aaron Curley <accwebs@gmail.com>
2025-09-21 16:36:41 -0500

    Add state data migration logic for 0.0.1 to 0.0.2

--- src/control/custom_alert_state.lua
+++ src/control/custom_alert_state.lua
@@ -24,6 +24,32 @@ end
 
 function custom_alert_state.update_state_version(current_file_mod_state_version --[[uint]])
     -- constants.TARGET_MOD_STATE_VERSION has "target" version we are upgrading to
+    if current_file_mod_state_version == 1 then
+        -- clear prior storage variables that no longer exist
+        storage.last_scan_tick = nil
+        storage.pending_plays = nil
+
+        -- convert structure of storage.force_info
+        local new_force_info_table = {}
+        for force_index, state_by_alert_type in pairs(storage.force_info) do
+            for alert_type, this_state_by_planet_name in pairs(state_by_alert_type) do
+                -- remove accidentally-created prior 'key' table; convert all others
+                if alert_type ~= 'custom_alert_status_by_type' then
+                    for planet_name, current_plays_list in pairs(this_state_by_planet_name) do
+                        if not new_force_info_table[force_index] then
+                            new_force_info_table[force_index] = {}
+                        end
+                        new_force_info_table[force_index][planet_name] = {
+                            current_plays = current_plays_list,
+                            new_play_since_tick = false,
+                            pending_plays = {}
+                        }
+                    end
+                end
+            end
+        end
+        storage.force_info = new_force_info_table
+    end
 end
 
 function custom_alert_state.on_global_disable()
